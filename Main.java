69ddf4c234f38c203f9e2ab721cbc4f97d0df460
JACKATTACK9 <jf1327@pleasantonusd.net>
2025-10-21 23:33:55 +0000

    SIX SEVEN!!!!!

--- Main.java
+++ Main.java
@@ -7,21 +7,140 @@ public class Main implements Directions {
   public static void main(String[] args) 
 	{
 		//My robot's name is karel
-    Robot karel = new Robot(5,1,East,9);
+    Robot karel = new Robot(1,1,East,20);
     World.setVisible(true);
-    World.setSize(10,10);
-    World.setDelay(100);
+    World.setSize(20,20);
+    World.setDelay(10);
 
 		karel.move();
     karel.putBeeper();
     karel.move();
     karel.move();
-    karel.turnLeft();
-
+    karel.move();
     karel.move();
     karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.move();
+    karel.move();
     karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    turnRight(karel);
+    karel.move();
+    karel.putBeeper();
+    turnRight(karel);
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.move();
+    for (int i=0;i<5;i++){
+      karel.move();
+      
+    }
+
+
+    
+
+
+
+
+    
+    Robot sixseven = new Robot(5,5, North,100);
+    sixseven.move();
+    sixseven.move();
+    sixseven.move();
+    sixseven.move();
+    sixseven.move();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    turnRight(sixseven);
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.turnLeft();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.turnLeft();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.turnLeft();
+    sixseven.move();
+    sixseven.move();
+    sixseven.putBeeper();
+    sixseven.move();
+    sixseven.move();
 
   }
     
+
+  public static void turnRight(Robot karel){
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.turnLeft();
+  }
+
+
+
 }
